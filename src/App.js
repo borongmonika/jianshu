4330bb36e48e4b6f022337903759a81728fcb591
borongmonika <15100377989@163.com>
2019-04-16 15:55:43 +0800

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component, Fragment } from 'react';
-import GlobalStyle from './style.js'
+import GlobalStyle from './style.js';
+import Iconfont from './statics/iconfont/iconfont.js'
 import Header from './common/header/index'
 class App extends Component {
   render() {
     return (
       <Fragment>
       <GlobalStyle/>
-      <div className='dell'><Header/></div>
+      <Iconfont/>
+      <Header/>
       </Fragment>
     )
   }
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,11 +1,75 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group'
+import {
+  HeaderWrapapper,
+  Logo,
+  Nav,
+  NavItem,
+  NavSearch,
+  Addition,
+  Button,
+  SearchWrapper
+} from './style';
 
 class Header extends Component {
-    render() {
-        return (
-            <div>jojo</div>
-        )
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      focused: false
     }
+    this.handelInputFocus = this.handelInputFocus.bind(this)
+    this.handelInputBlur = this.handelInputBlur.bind(this)
+  }
+  render() {
+    return (
+      <HeaderWrapapper>
+        <Logo href='./' />
+        <Nav>
+          <NavItem className='left active'>首页</NavItem>
+          <NavItem className='left'>下载App</NavItem>
+          <NavItem className='right'>登录</NavItem>
+          <NavItem className='right'>
+            <span className='iconfont'>&#xe636;</span>
+          </NavItem>
+          <SearchWrapper>
+            <CSSTransition 
+              in={this.state.focused}
+              timeout={200}
+              classNames="slide"
+            >
+              <NavSearch
+                className={this.state.focused ? 'focused' : ''}
+                onFocus={this.handelInputFocus}
+                onBlur={this.handelInputBlur}
+              >
+              </NavSearch>
+            </CSSTransition>
+            <span className=
+              {this.state.focused ? 'focused iconfont' : 'iconfont'}>&#xe637;
+              </span>
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className='writting'>
+            <span className='iconfont'>&#xe616;</span>文章
+                    </Button>
+          <Button className='reg'>注册</Button>
+        </Addition>
+      </HeaderWrapapper>
+    )
+
+  }
+  handelInputFocus() {
+    this.setState({
+      focused: true
+    })
+  }
+  handelInputBlur() {
+    this.setState({
+      focused: false
+    })
+  }
 }
 
 export default Header;

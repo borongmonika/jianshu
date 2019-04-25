ba41392b00985e34890d3a734ab16c3a2286cbcd
borongmonika <15100377989@163.com>
2019-04-25 09:13:21 +0800

    page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Iconfont from './statics/iconfont/iconfont.js'
 import Header from './common/header/index';
 import store from './store';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Detail from './pages/detail';
 
 class App extends Component {
   render() {
@@ -12,6 +15,10 @@ class App extends Component {
         <GlobalStyle/>
         <Iconfont/>
         <Header/>
+        <BrowserRouter>
+          <Route path='/' exact component={Home}></Route>
+          <Route path='/detail' exact component={Detail}></Route>
+        </BrowserRouter>
       </Provider>
     )
   }
---
+++ src/pages/home/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const HomeWrapper = styled.div`
+    overflow: hidden;
+    width: 960px;
+    margin: 0 auto;
+`
+export const HomeRight = styled.div`
+    width: 240px;
+    float: right;
+`
+export const HomeLeft = styled.div`
+    margin-left: 15px;
+    padding-top: 30px;
+    width: 625px;
+    float: left;
+    .banner-img {
+        width: 625px;
+        height: 270px;
+    }
+`
